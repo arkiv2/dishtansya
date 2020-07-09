4d8be0f28756347d0c8762a944dbff7651adf5a9
Arki Valencia II <valenciaarkiii@gmail.com>
2020-07-09 03:03:26 +0000

    Added login method to auth controller

--- app/Http/Controllers/API/Authentication/AuthController.php
+++ app/Http/Controllers/API/Authentication/AuthController.php
@@ -5,20 +5,45 @@ namespace App\Http\Controllers\API\Authentication;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $validated = $request->validate([
-            'email' => 'email|required|unique:users',
+        try
+        {
+            $validated = $request->validate([
+                'email' => 'email|required|unique:users',
+                'password' => 'required',
+            ]);
+            $validated['password'] = bcrypt($request->input('password'));
+            User::create($validated);
+        }
+        catch(ValidationException $e)
+        {
+            if($e->getMessage() == 'The given data was invalid.')
+            {
+                return response()->json(['message' => 'Email already taken'], 400);
+            }
+        }
+
+        return response()->json(['message' => 'User successfully registered'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $authData = $request->validate([
+            'email' => 'email|required',
             'password' => 'required',
         ]);
 
-        $validated['password'] = bcrypt($request->input('password'));
+        if(!auth()->attempt($authData))
+        {
+            return response()->json(['message' => 'Invalid Credentials']);
+        }
 
-        User::create($validated);
-
-        return response()->json(['message' => 'User successfully registered'], 201);
+        $token = auth()->user()->createToken('authToken')->accessToken;
+        return response()->json(['access_token' => $token], 201);
     }
 }
