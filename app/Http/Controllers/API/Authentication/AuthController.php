4c352f9171cc1de1cba979096237676cad240120
Arki Valencia II <valenciaarkiii@gmail.com>
2020-07-09 03:48:03 +0000

    Feat: Account Locking
    Added Login Throttling for invalid login attempts. Accounts are locked for five minutes after five failed attempts

--- app/Http/Controllers/API/Authentication/AuthController.php
+++ app/Http/Controllers/API/Authentication/AuthController.php
@@ -5,10 +5,29 @@ namespace App\Http\Controllers\API\Authentication;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 
 class AuthController extends Controller
 {
+    use ThrottlesLogins;
+
+    protected $maxAttempts = 5;
+    protected $decayMinutes = 5;
+    public function username()
+    {
+        return 'email';
+    }
+
+    protected function sendLockoutResponse(Request $request)
+    {
+        $seconds = $this->limiter()->availableIn(
+            $this->throttleKey($request)
+        );
+    }
+
     public function register(Request $request)
     {
         try
@@ -40,9 +59,16 @@ class AuthController extends Controller
 
         if(!auth()->attempt($authData))
         {
-            return response()->json(['message' => 'Invalid Credentials']);
+            if (method_exists($this, 'hasTooManyLoginAttempts') &&
+                $this->hasTooManyLoginAttempts($request)) {
+                $this->fireLockoutEvent($request);
+                return response()->json(['message' => 'Your Account is locked for ' . $this->decayMinutes . ' minutes'], 401);
+            }
+            $this->incrementLoginAttempts($request);
+            return response()->json(['message' => 'Invalid credentials'], 401);
         }
 
+        $this->clearLoginAttempts($request);
         $token = auth()->user()->createToken('authToken')->accessToken;
         return response()->json(['access_token' => $token], 201);
     }
--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -54,4 +54,68 @@ class AuthenticationTest extends TestCase
             ])
             ->assertStatus(201);
     }
+
+    public function test_it_does_not_login_users_with_incorrect_email()
+    {
+        $this->setupUser();
+
+        $login = ['email' => 'frontend@multisyscorp.com', 'password' => 'test1234'];
+
+        $this->json('POST', 'login', $login, ['Accept' => 'application/json'])
+            ->assertJson([
+               'message' => 'Invalid credentials',
+            ])
+            ->assertStatus(401);
+    }
+
+    public function test_it_does_not_login_users_with_incorrect_password()
+    {
+        $this->setupUser();
+
+        $login = ['email' => 'backend@multisyscorp.com', 'password' => 'test6666'];
+
+        $this->json('POST', 'login', $login, ['Accept' => 'application/json'])
+            ->assertJson([
+                'message' => 'Invalid credentials',
+            ])
+            ->assertStatus(401);
+    }
+
+    public function test_it_locks_account_for_five_minutes_after_five_failed_attempts()
+    {
+        $this->setupUser();
+
+        $login = ['email' => 'backend@multisyscorp.com', 'password' => 'test6666'];
+        $this->json('POST', 'login', $login, ['Accept' => 'application/json'])
+            ->assertJson([
+                'message' => 'Invalid credentials',
+            ])
+            ->assertStatus(401);
+        $this->json('POST', 'login', $login, ['Accept' => 'application/json'])
+            ->assertJson([
+                'message' => 'Invalid credentials',
+            ])
+            ->assertStatus(401);
+        $this->json('POST', 'login', $login, ['Accept' => 'application/json'])
+            ->assertJson([
+                'message' => 'Invalid credentials',
+            ])
+            ->assertStatus(401);
+        $this->json('POST', 'login', $login, ['Accept' => 'application/json'])
+            ->assertJson([
+                'message' => 'Invalid credentials',
+            ])
+            ->assertStatus(401);
+        $this->json('POST', 'login', $login, ['Accept' => 'application/json'])
+            ->assertJson([
+                'message' => 'Invalid credentials',
+            ])
+            ->assertStatus(401);
+        $this->json('POST', 'login', $login, ['Accept' => 'application/json'])
+            ->assertJson([
+                'message' => 'Your Account is locked for 5 minutes',
+            ])
+            ->assertStatus(401);
+
+    }
 }
