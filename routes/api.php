017c7cf6094305304ae110a45e340198aab53db8
Arki Valencia II <valenciaarkiii@gmail.com>
2020-07-09 03:02:51 +0000

    Added login route

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('register', 'API\Authentication\AuthController@register');
+Route::post('login', 'API\Authentication\AuthController@login');
