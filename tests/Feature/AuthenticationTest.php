08c7da81ead81a7e34e7e7ec5b12829fdb627c7b
Arki Valencia II <valenciaarkiii@gmail.com>
2020-07-09 01:48:24 +0000

    Added test for user registration
    Check if user can successfully register

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,21 +2,33 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase, DatabaseMigrations;
 
-        $response->assertStatus(200);
+    public function test_it_registers_user_successfully()
+    {
+        $user = ['email' => 'backend@multisyscorp.com', 'password' => 'test1234'];
+
+        $this->json('POST', 'register', $user, ['Accept' => 'application/json'])
+            ->assertJson([
+               'message' => 'User successfully registered',
+            ])
+            ->assertStatus(201);
+    }
+
+    public function test_it_does_not_accept_duplicate_emails()
+    {
+        $user = new User();
+
+
+        $user = ['email' => 'backend@multisyscorp.com', 'password' => 'test1234'];
+
     }
 }
